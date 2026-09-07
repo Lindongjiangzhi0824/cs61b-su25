e6778b13e5a02550a23f7d0bf96fb72c118f0733
TOGeeks <157305486@qq.com>
2026-09-07 10:34:36 +0800

    Completed ArrayExercices

--- lab02/src/ArrayExercises.java
+++ lab02/src/ArrayExercises.java
@@ -1,19 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class ArrayExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        return new int[]{1, 2, 3, 4, 5, 6};
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int min = array[0], max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return max - min;
     }
 
 }
