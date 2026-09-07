e4f1f07f6a59d67f68da69ac9c2f3b0ec431506e
TOGeeks <157305486@qq.com>
2026-09-07 11:26:07 +0800

    Fix TriangleDrawer2 new line problem.

--- lab02/src/TriangleDrawer2.java
+++ lab02/src/TriangleDrawer2.java
@@ -1,7 +1,7 @@
 public class TriangleDrawer2 {
     public static void drawTriangle() {
         int SIZE = 5;
-        for (int i = 0; i <= SIZE; i++) {
+        for (int i = 1; i <= SIZE; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
