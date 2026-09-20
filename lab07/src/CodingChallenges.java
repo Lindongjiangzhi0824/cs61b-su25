57379987e566907821d24135d2498906e4421153
TOGeeks <157305486@qq.com>
2026-09-20 11:02:09 +0800

    Completed Exercise CodingChallenges

--- lab07/src/CodingChallenges.java
+++ lab07/src/CodingChallenges.java
@@ -1,9 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class CodingChallenges {
 
@@ -12,7 +7,17 @@ public class CodingChallenges {
      * values from 0 to N except for one missing number.
      */
     public static int missingNumber(int[] values) {
-        // TODO
+        boolean[] exists = new boolean[values.length];
+        for(int i = 0; i < values.length; i++) {
+            if(values[i] < values.length) {
+                exists[values[i]] = true;
+            }
+        }
+        for(int i = 0; i < values.length; i++) {
+            if(!exists[i]) {
+                return i;
+            }
+        }
         return -1;
     }
 
@@ -21,7 +26,20 @@ public class CodingChallenges {
      * permutation of s2 if it has the same number of each character as s2.
      */
     public static boolean isPermutation(String s1, String s2) {
-        // TODO
-        return false;
+        if(s1.length() != s2.length()) {
+            return false;
+        }
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for(int i = 0; i < s1.length(); i++) {
+            if(arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- lab07/test/CodingChallengesTest.java
+++ lab07/test/CodingChallengesTest.java
@@ -6,11 +6,26 @@ public class CodingChallengesTest {
 
     @Test
     public void testMissingNumber() {
-	// TODO
+        int arr1[] = {1,2,4,5,0};
+	    assertWithMessage("Array [1,2,4,5,0] should get 3.").that(CodingChallenges.missingNumber(arr1)).isEqualTo(3);
+
+        int arr2[] = {1,2,3,4};
+        assertWithMessage("The array [1,2,3,4] should be missing 0.").that(CodingChallenges.missingNumber(arr2)).isEqualTo(0);
+
+        int arr3[] = {};
+        assertWithMessage("Empty array should be missing 0.").that(CodingChallenges.missingNumber(arr3)).isEqualTo(-1);
+
     }
 
     @Test
     public void testIsPermutation() {
-	// TODO
+	    String s1 = "abc", s2 = "cba";
+        assertWithMessage("They are all composed of one element from each of a, b, and c.").that(CodingChallenges.isPermutation(s1, s2)).isTrue();
+
+        String s3 = "silent", s4 = "listen";
+        assertWithMessage("Both are consist of s i l e n t only once.").that(CodingChallenges.isPermutation(s3, s4)).isTrue();
+
+        String s5 = "hello", s6 = "apple";
+        assertWithMessage("Not match.").that(CodingChallenges.isPermutation(s5, s6)).isFalse();
     }
 }
