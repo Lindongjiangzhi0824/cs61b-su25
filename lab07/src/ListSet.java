d90b50581f08cc99b0580fafae715792bf16d875
TOGeeks <157305486@qq.com>
2026-09-20 20:11:11 +0800

    Completed Exercise: Implementing Sets

--- lab07/src/ListSet.java
+++ lab07/src/ListSet.java
@@ -15,14 +15,22 @@ public class ListSet implements SimpleSet {
     /** Adds k to the set. */
     @Override
     public void add(int k) {
-        // TODO: Implement this method.
+        if (!elems.contains(k)) {
+            elems.add(k);
+        } else {
+            System.out.println(k + " already exists !");
+        }
     }
 
     /** Removes k from the set. */
     @Override
     public void remove(int k) {
         Integer toRemove = k;
-        // TODO - use the above variable with an appropriate List method.
+        if (contains(toRemove)) {
+            elems.remove(toRemove);
+        } else {
+            System.out.println("Elems not exist " + k + " .");
+        }
         // The reason is beyond the scope of this lab, but involves
         // method resolution.
     }
@@ -30,27 +38,34 @@ public class ListSet implements SimpleSet {
     /** Return true if k is in this set, false otherwise. */
     @Override
     public boolean contains(int k) {
-        // TODO: Implement this method.
+        for (int x : elems) {
+            if (k == x) {
+                return true;
+            }
+        }
         return false;
     }
 
     /** Return true if this set is empty, false otherwise. */
     @Override
     public boolean isEmpty() {
-      return this.size() == 0;
+        return this.size() == 0;
     }
 
     /** Returns the number of items in the set. */
     @Override
     public int size() {
-        // TODO: Implement this method.
-        return 0;
+        return elems.size();
     }
 
     /** Returns an array containing all of the elements in this collection. */
     @Override
     public int[] toIntArray() {
-        // TODO - use a for loop!
-        return null;
+        int[] arr = new int[elems.size()];
+        int index = 0;
+        for (int x : elems) {
+            arr[index++] = x;
+        }
+        return arr;
     }
 }
